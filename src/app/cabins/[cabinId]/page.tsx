fa45fcc42846061662fac10637a7452c7500594a
bear320 <aaabear320@gmail.com>
2024-09-05 21:55:19 +0800

    feat: 🎸 Use client component in server component

--- src/app/cabins/[cabinId]/page.tsx
+++ src/app/cabins/[cabinId]/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import TextExpander from "@/app/_components/TextExpander";
 
 export const generateMetadata = async ({ params }: { params: any }) => {
   const { name } = await getCabin(+params.cabinId);
@@ -37,7 +38,9 @@ const Page = async ({ params }: { params: { cabinId: string } }) => {
             Cabin {name}
           </h3>
 
-          <p className="mb-10 text-lg text-primary-300">{description}</p>
+          <p className="mb-10 text-lg text-primary-300">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="mb-7 flex flex-col gap-4">
             <li className="flex items-center gap-3">
