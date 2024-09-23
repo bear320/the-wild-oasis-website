cbf5a57088d43d1e6f6e1978e7ebaf4686beb6a1
bear320 <aaabear320@gmail.com>
2024-09-23 21:32:32 +0800

    feat: 🎸 Use useFormStatus to display a loading indicator

--- src/app/_components/UpdateProfileForm.tsx
+++ src/app/_components/UpdateProfileForm.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { IGuest } from "../types";
+import { useFormStatus } from "react-dom";
 import { updateGuest } from "../_lib/actions";
+import { IGuest } from "../types";
 
 const UpdateProfileForm = ({
   guest,
@@ -60,11 +61,22 @@ const UpdateProfileForm = ({
       </div>
 
       <div className="flex items-center justify-end gap-6">
-        <button className="bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+};
+
+const Button = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   );
 };
 
