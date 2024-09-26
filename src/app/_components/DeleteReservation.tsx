12e49592da7c7484d2676187446e2ee4539cd582
bear320 <aaabear320@gmail.com>
2024-09-26 20:07:46 +0800

    feat: 🎸 Use useTransition to show loading indicator

--- src/app/_components/DeleteReservation.tsx
+++ src/app/_components/DeleteReservation.tsx
@@ -1,16 +1,34 @@
 "use client";
 
+import { useTransition } from "react";
 import { deleteReservation } from "../_lib/actions";
 import { TrashIcon } from "@heroicons/react/24/solid";
+import { Span } from "next/dist/trace";
+import SpinnerMini from "./SpinnerMini";
 
 const DeleteReservation = ({ bookingId }: { bookingId: number }) => {
+  const [isPending, startTransition] = useTransition();
+
+  const handleDelete = () => {
+    if (confirm("Are you sure you want to delete this reservation?"))
+      startTransition(() => deleteReservation(bookingId));
+  };
+
   return (
     <button
       className="group flex flex-grow items-center gap-2 px-3 text-xs font-bold uppercase text-primary-300 transition-colors hover:bg-accent-600 hover:text-primary-900"
-      onClick={() => deleteReservation(bookingId)}
+      onClick={handleDelete}
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 transition-colors group-hover:text-primary-800" />
-      <span className="mt-1">Delete</span>
+      {!isPending ? (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 transition-colors group-hover:text-primary-800" />
+          <span className="mt-1">Delete</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 };
